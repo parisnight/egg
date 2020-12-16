cface48ed44d211d007688a3399cfa0357f9b30a
parisnight <parisnight@softhome.net>
2020-12-16 08:50:32 -0500

    Koch line and snowflake working

---
+++ ezx.scm
@@ -0,0 +1,89 @@
+;;;;; ezxdisp.scm          guile interface to ezxdisp 2020.11.5
+;;;;; gcc -I../../include -lX11 -shared -o libezx.so -fPIC ezxdisp.c
+
+(define-module (ezxdisp ezxdisp)
+  #:use-module (system foreign)
+  #:export (ezx-quit))
+
+;(define ezxdisp (dynamic-link "/home/fuzz/Downloads/ezxdisp-0.1.4/src/x11/libezx.so"))
+
+(define ezx-init
+  (pointer->procedure '*
+                      (dynamic-func "ezx_init" ezxdisp)
+                      (list int int '*)))
+
+(define ezx-quit
+  (pointer->procedure void
+                      (dynamic-func "ezx_quit" ezxdisp)
+                      (list '*)))
+
+(define ezx-set-background
+  (pointer->procedure void
+                      (dynamic-func "ezx_set_background" ezxdisp)
+                      (list '* '*)))
+
+(define ezx-redraw
+  (pointer->procedure void
+                      (dynamic-func "ezx_redraw" ezxdisp)
+                      (list '*)))
+
+(define ezx-line-2d
+  (pointer->procedure void
+                      (dynamic-func "ezx_line_2d" ezxdisp)
+                      (list '* int int int int '* int)))
+
+(define ezx-line
+  (pointer->procedure void
+                      (dynamic-func "ezx_line" ezxdisp)
+                      (list '* int int int int '* int)))
+
+(define ezx-lines-2d-lolevel
+  (pointer->procedure void
+                      (dynamic-func "ezx_lines_2d_lolevel" ezxdisp)
+                      (list '* '* int '* int)))
+
+(define (ezx-lines-2d ezx points color width)
+  (ezx-lines-2d-lolevel ezx points (s32vector-length points) color width))
+
+(define (ezx-poly-2d ezx points color)
+  (ezx-poly-2d-lolevel ezx points (s32vector-length points) color))
+  
+(define (ezx-poly-3d ezx points hx hy hz color)
+  (ezx-poly-3d-lolevel ezx points hx hy hz (s32vector-length points) color))
+
+(use-modules (rnrs bytevectors))
+
+(define (make-ezx-color r g b) (f64vector r g b) )  ; SRFI-4
+
+;(define ezx-next-event get-next-event)
+;(ezx-set-background ezx (make-ezx-color 1 1 1))
+;(ezx-lines-2d ezxp (scm->pointer points) (scm->pointer (make-ezx-color 1 0 0)) 4)
+;(ezx-lines-2d-lolevel ezxp (scm->pointer points) 10 (scm->pointer (make-ezx-color 1 0 0)) 4)
+;  (define str (bytevector->pointer (string->utf8 "hello\0")))
+;  (define points (s32vector 2 2 42 2 42 42 2 42 2 2))
+;  (bytevector->pointer (s32-list->bytevector '(2 2 42 2 42 42 2 42 2 2))))
+;  (bytevector->pointer (u8-list->bytevector '(255 0 255))))
+
+(define ezxp (ezx-init 200 100 (string->pointer "goodbye")))
+
+(define co (make-ezx-color 0 1 1))
+
+(define mybv (make-bytevector 24))
+(bytevector-ieee-double-native-set! mybv 0 1.0))
+(define mybvp (bytevector->pointer mybv))
+
+(ezx-line-2d ezxp 0 0 100 20 mybvp 2)
+(ezx-line ezxp 0 0 100 40 mybvp 2)
+(ezx-set-background ezxp (bytevector->pointer co))
+(ezx-redraw ezxp)
+
+(define (fill-bv bv l n)
+  (match l
+	 (() bv)
+	 (((x y) . r)
+	  (s32vector-set! bv n x)
+	  (s32vector-set! bv (+ n 1) y)
+	  (fill-bv bv r (+ 2 n)))))
+
+     (fill-bv (make-s32vector (* count 2)) (list 1 2 3 4 5 6) 0)
+  
---
+++ turtle.scm
@@ -0,0 +1,166 @@
+;;;;; A. V. Hershey font   2020.12.10
+
+(load "fbgraph.scm")
+(use-modules (fbgraph fbgraph)
+	     (system foreign)
+	     (rnrs bytevectors))
+
+(define cursx 100)
+(define cursy 100)
+(define i 0)
+(define final 0)
+(define lines (make-s16vector 600))
+(define oldcoord 0)
+
+(define (append-coord i x)
+  (if (even? i)
+      (set! x (+ x cursx))
+      (set! x (+ x cursy)))
+  (s16vector-set! lines i x))
+
+(define (coord c)
+  (- (char->integer c) (char->integer #\R)))
+
+(define (hershey-encoded-out str)
+  ;;(string->number (string-trim (substring mys 0 5)))
+  ;;(string->number (string-trim (substring mys 5 8)))
+  (set! cursx (- cursx (coord (string-ref str 8))))
+  (set! final (coord (string-ref str 9)))
+  (set! i 0)
+  (set! oldcoord 0)
+  (string-for-each
+   (lambda (c)
+     (set! c (- (char->integer c) (char->integer #\R)))
+     ;;(display c) (display " ")
+     (if (and (= -50 oldcoord) (= 0 c))
+	 (begin
+	   (fblines (bytevector->pointer lines) (quotient i 2))
+	   ;;(display lines)
+	   ;;(display (quotient i 2))
+	   (set! i -1))  ;; get rid of the appended -50
+	 (append-coord i c))
+     (set! oldcoord c)
+     (set! i (1+ i)))
+   (substring str 10))
+  (fblines (bytevector->pointer lines) (quotient i 2))
+  (set! cursx (+ final cursx))
+  ;;(display lines)
+  )
+
+(define (gr)
+  (fbopen)
+  (bytevector-u32-native-set! color 0 #xffff00)
+  (fblines (bytevector->pointer (s16vector 4 4 4 40 40 40 40 4 4 4)) 4)
+  ;;  (fbclose)
+  )
+
+(define rowmans
+ "  699  1JZ
+  714  9MWRFRT RRYQZR[SZRY
+  717  6JZNFNM RVFVM
+  733 12H]SBLb RYBRb RLOZO RKUYU
+ 2246 24F^IUISJPLONOPPTSVTXTZS[Q RISJQLPNPPQTTVUXUZT[Q[O
+  718 14KYQFOGNINKOMQNSNUMVKVIUGSFQF" )
+
+(define typeface (string-split rowmans #\newline))
+(define mychar (list-ref typeface 3))
+;;(gr)
+;;(hershey-encoded-out mychar)
+(define (hprint s)
+  (string-for-each
+   (lambda (c)
+     (hershey-encoded-out (list-ref typeface (- (char->integer c) 30))))
+   s))
+
+;;(hprint "Merry-Christmas")
+
+(define a 0)
+(define oldx 100)
+(define oldy 100)
+
+(define (moveto x y)
+  (fblines (bytevector->pointer lines) (quotient i 2))
+  (set! oldx x)
+  (set! oldy y)
+  (set! i 0))
+
+(define (render)
+  (fblines (bytevector->pointer lines) (quotient i 2))
+  (set! i 0))
+
+(define (drawto x y)
+  (append-coord i x)
+  (set! i (1+ i))
+  (set! oldx x)
+  (append-coord i y)
+  (set! i (1+ i))
+  (set! oldy y))
+
+(define (rdrawto x y)
+  (set! x (+ oldx x))
+  (set! y (+ oldy y))
+  (append-coord i x)
+  (set! i (1+ i))
+  (set! oldx x)
+  (append-coord i y)
+  (set! i (1+ i))
+  (set! oldy y))
+
+(define (tortoise-turn angle)
+  (set! a (+ a (/ angle 57.3))))
+
+(define (tortoise-move d)
+  (let ((sa (sin a))
+	(ca (cos a)))
+    (rdrawto
+     (inexact->exact (round (+ (* d ca) (- 0 (* d sa)))))
+     (inexact->exact (round (+ (* d sa) (* d ca)))))))
+
+(define (draw-polygon! circumference vertices)
+  (let ((side (/ circumference vertices))
+	(angle (/ 360 vertices)))
+    (let iterate ((i 1))
+      (if (<= i (1+ vertices))
+	  (begin
+	    (tortoise-move side)
+	    (tortoise-turn angle)
+	    (iterate (1+ i)))))))
+     
+;; (draw-polygon! 86 6)
+;; (render)
+     
+;; (tortoise-penup)
+;; (tortoise-move 1)
+;; (tortoise-turn 30)
+;; (tortoise-pendown)
+;; (draw-polygon! 12 3)
+     
+;; (tortoise-penup)
+;; (tortoise-move -2)
+;; (tortoise-turn -100)
+;; (tortoise-pendown)
+;; (draw-polygon! 10 64)
+
+(define (koch-line length depth)
+  (if (zero? depth)
+      (tortoise-move length)
+      (let ((sub-length (/ length 3))
+	    (sub-depth (1- depth)))
+	(for-each (lambda (angle)
+		    (koch-line sub-length sub-depth)
+		    (tortoise-turn angle))
+		  '(60 -120 60 0)))))
+     
+(define (snowflake length depth sign)
+  (let iterate ((i 1))
+    (if (<= i 3)
+	(begin
+	  (koch-line length depth)
+	  (tortoise-turn (* sign -120))
+	  (iterate (1+ i))))))
+     
+;;     (snowflake 80 3 1)
+;;     (tortoise-turn 180)
+;;(render)
+;;     (snowflake 80 3 -1)
+;;(render)
