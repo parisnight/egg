271859ff2aa630729900f71e0463117a2583a6be
parisnight <parisnight@softhome.net>
2020-12-20 09:03:54 -0500

    Swap byte ordering in alpha rgb conversion utility

--- argbtorgb.c
+++ argbtorgb.c
@@ -2,15 +2,23 @@
 
 #include <stdio.h>
 
-void main(int argc, char **argv) {
-FILE *ifil,*ofil;
-int i=0,c;
+void main(int argc, char **argv)
+{
+  FILE *ifil, *ofil;
+  int i = 0, j, c, b[4];
 
-ifil=fopen(argv[1],"rb");
-ofil=fopen("out","wb");
-while ((c=getc(ifil))!=EOF)
+  ifil = fopen(argv[1], "rb");
+  ofil = fopen("out", "wb");
+/*while ((c=getc(ifil))!=EOF)
  if ((i++ %4) != 3) putc(c,ofil);
+*/
+
+  while ((c = getc(ifil)) != EOF) {
+    if ((i++ % 4) == 3)
+      for (j = 0; j < 3; j++)
+        putc(b[j], ofil);
+    else
+      b[3 - (i % 4)] = c;       /* swap byte ordering */
+  }
 
 }
-
-
